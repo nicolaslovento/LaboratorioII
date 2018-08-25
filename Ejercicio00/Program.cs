f5d4a34a654a9e838e1d8566cfb4042a1ab20c36
nicolaslovento <31898579+nicolaslovento@users.noreply.github.com>
2018-08-25 12:34:04 -0300

    pido datos y muestro

---
+++ Ejercicio00/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio00
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            string nombre, apellido;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre=Console.ReadLine();
+
+            Console.Write("Ingrese su apellido: ");
+            apellido = Console.ReadLine();
+
+            Console.Write("Su nombre es {0} y su apellido {1}", nombre, apellido);
+            Console.Read();
+
+
+
+
+        }
+	}
+}
