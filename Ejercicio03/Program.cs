25f2c64a38e600c9aa5c8dc968c793d3bc28fa75
nicolaslovento <31898579+nicolaslovento@users.noreply.github.com>
2018-08-25 13:00:36 -0300

    num primos v1

---
+++ Ejercicio03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio03
+{
+    class Program
+    {/*Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario
+por consola.*/
+        static void Main(string[] args)
+        {
+            int numero;
+            int i;
+            int contador = 0;
+            Console.Write("Ingrese numero: ");
+            numero=int.Parse(Console.ReadLine());
+
+            for (i = numero; i > 1; i--)
+            {
+                for(int j=1; j<=i;j++)
+                {
+                    if(i%j==0)
+                    {
+                        contador++;
+                    }
+
+                }
+
+                if (contador == 2)
+                {
+                    Console.Write("{0}\n", i);
+                }
+
+                contador = 0;
+            }
+            Console.Read();
+        }
+    }
+}
