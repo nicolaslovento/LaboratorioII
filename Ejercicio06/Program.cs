3538a1bca0677db1cb06780026c65a752cd4d6e8
Nicolas <nicolaslovento.94@hotmail.com>
2018-08-25 13:14:33 -0300

    años bisiestos v1

---
+++ Ejercicio06/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+
+            Console.Write("Ingrese año: ");
+            bool esNumero = int.TryParse(Console.ReadLine(), out año);
+
+            if (esNumero)
+            {
+                if (año % 4 == 0)
+                {
+                    if (año % 100 == 0)
+                    {
+                        if (año % 400 == 0)
+                        {
+                            Console.WriteLine("{0} Es bisiesto!",año);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} NO es bisiesto!", año);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} Es bisiesto!", año);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} NO es bisiesto!", año);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
