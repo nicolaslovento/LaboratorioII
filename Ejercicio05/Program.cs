fdc127ba6b0de6d283e09db4fc8aeb5c4641a01e
Nicolas <nicolaslovento.94@hotmail.com>
2018-08-25 13:15:51 -0300

    No terminado

--- Ejercicio05/Program.cs
+++ Ejercicio05/Program.cs
@@ -14,7 +14,7 @@ El primer centro numérico es el 6, el cual separa la lista (1 a 8) en los grupo
 cuyas sumas son ambas iguales a 15. El segundo centro numérico es el 35, el cual separa la lista (1 a
 49) en los grupos: (1 a 34) y (36 a 49) cuyas sumas son ambas iguales a 595.
 Se pide elaborar una aplicación que calcule los centros numéricos entre 1 y el número que el
-usuario ingrese por consola.
+usuario ingrese por consola
 Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).*/
         static void Main(string[] args)
         {
