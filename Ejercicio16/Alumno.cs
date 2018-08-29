a12e0837b40429655cecc318b21f50f02e0a4cf1
Nicolas <nicolaslovento.94@hotmail.com>
2018-08-29 00:10:06 -0300

    objeto:alumnos v1

---
+++ Ejercicio16/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Alumno
+    {
+        private byte _nota1;
+        private byte _nota2;
+        private byte _notaFinal;
+        public string nombre;
+        public string apellido;
+        public int legajo;
+        Random rnd = new Random();
+        public Alumno(string nombre,string apellido,int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void Estudiar(byte nota1,byte nota2)
+        {
+            this._nota1 = nota1;
+            this._nota2 = nota2;
+        }
+
+        public void CalcularFinal()
+        {
+            if (this._nota1 < 4 || this._nota2 < 4)
+            {
+                this._notaFinal = 1;
+            }else
+            {
+                this._notaFinal =(byte)rnd.Next(1,10);
+            }
+        }
+
+        public void Mostrar()
+        {
+            Console.Write("Nombre: {0}\nApellido: {1}\nLegajo: {2}\nNota 1: {3}\nNota 2: {4}\nNota Final: {5}\n\n", this.nombre, this.apellido, this.legajo, this._nota1, this._nota2, this._notaFinal);
+        }
+    }
+}
---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Nicolas", "Rodriguez", 190);
+            Alumno alumno2 = new Alumno("Roberto", "Jaime", 191);
+            Alumno alumno3 = new Alumno("Micke", "Jhonson", 192);
+
+            alumno1.Estudiar(6, 7);
+            alumno2.Estudiar(1, 1);
+            alumno3.Estudiar(5, 3);
+
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            alumno1.Mostrar();
+            alumno2.Mostrar();
+            alumno3.Mostrar();
+            Console.ReadKey();
+        }
+    }
+}
