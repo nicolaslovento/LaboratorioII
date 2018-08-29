063a07eb7438cf49ec4303f7727c6fe496f79fdc
Nicolas <nicolaslovento.94@hotmail.com>
2018-08-28 23:14:52 -0300

    calculador areas v1

---
+++ Ejercicio14/CalculoDeArea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    /*
+     a. double CalcularCuadrado(double)
+b. double CalcularTriangulo(double, double)
+c. double CalcularCirculo(double)*/
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double lado)
+        {
+            return Math.Pow(lado, 2);
+        }
+
+        public static double CalcularCirculo(double radio)
+        {
+            double pi = 3.14;
+            double resultado=pi*radio*radio;
+
+            return resultado;
+        }
+        public static double CalcularTriangulo(double base1,double altura)
+        {
+            double resultado=(base1*altura)/2;
+
+            return resultado;
+        }
+    }
+}
---
+++ Ejercicio14/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio14
+{
+    class Program
+    { /*Realizar una clase llamada CalculoDeArea que posea 3 métodos de clase (estáticos) que realicen el
+cálculo del área que corresponda:
+a. double CalcularCuadrado(double)
+b. double CalcularTriangulo(double, double)
+c. double CalcularCirculo(double)
+El ingreso de los datos como la visualización se deberán realizar desde el método Main().*/
+        static void Main(string[] args)
+        {
+            int opcion;
+            char seguir='s';
+            double lado, radio, base1, altura;
+            while(seguir=='s')
+            { 
+            Console.Write("1.Calcular area cuadrado.\n2.Calcular area Triangulo.\n3.Calcular area circulo.\nIngrese Opcion: ");
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write("Ingrese lado: ");
+                    lado = double.Parse(Console.ReadLine());
+                    Console.Write("{0}", CalculoDeArea.CalcularCuadrado(lado));
+                    break;
+                case 2:
+                    Console.Write("Ingrese Base: ");
+                    base1 = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese Altura: ");
+                    altura = double.Parse(Console.ReadLine());
+                    Console.Write("{0}", CalculoDeArea.CalcularTriangulo(base1, altura));
+
+                    break;
+                case 3:
+                    Console.Write("Ingrese Radio: ");
+                    radio = double.Parse(Console.ReadLine());
+                    Console.Write("{0}", CalculoDeArea.CalcularCirculo(radio));
+                    break;
+                default:
+                        seguir = 'n';
+                    break;
+
+            }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.ReadKey();
+            }
+
+        }
+    }
+
