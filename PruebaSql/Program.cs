016cb545ed67fec767ca54b21a82e987fbc72a91
alumno <alumno>
2018-11-06 12:10:56 -0300

    .

---
+++ PruebaSql/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PruebaSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Hello World!");
+        
+        /*Para aplicar consultas debo conectar con la base de datos*/
+
+        
+
+
+
+        }
+    }
+}
